1b266b80eb52250ec01c5f27abeaba494fdaec9e
MAIN\axp3579 <axp3579@rit.edu>
2017-04-05 11:45:22 -0400

    UI Stuff

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public bool mapOpen = false;
+
+    /*
+    //Stuff for references
+    public GameObject gameManager;
+    GameManager = GameObject.findgameobjectwithtag(Gman);
+    gameManager.GetComponent<GameManager>().FUNCTION_OR_VARIABLE
+    */
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //map toggle control
+        if (Input.GetKeyUp(KeyCode.M))
+        {
+            if (mapOpen)
+                CloseMap();
+            else
+                OpenMap();
+        }	
+	}
+
+
+
+    private void OpenMap()
+    {
+        //Check if camera can move??
+
+        //Get and store previous camera position (reference to camera?)
+
+        //move camera to map position
+
+        //remove player control
+
+    }
+
+    private void CloseMap()
+    {
+        //Return camera to stored position
+
+        //Return player control
+
+
+    }
+}
