c193f92b433ec04e1ec4d21de0fe0edc53bcd1dd
Aaron Romel <aromel8838@gmail.com>
2017-04-05 11:43:30 -0400

    Added plus and minus buttons to UI

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -38,7 +38,6 @@ public class Room : MonoBehaviour
         roomContents = new List<GameObject>();
         spawn = gameObject.GetComponentInChildren<SpawnPoint>();
 		gameManager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
-		converterBehavior = convertor.GetComponent<GameManager> ();
 
         //Find and add any generators or convetors to the room list.
         foreach (Transform child in transform)
